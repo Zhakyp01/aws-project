9727f9b98a6e37e7ae8a034d5410960e40e4141b
Zhakyp <73534500+Zhakyp01@users.noreply.github.com>
2023-03-29 17:36:24 +0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 # --- root/backend.tf ---
 
-# terraform {
-#   backend "s3" {
-#     bucket = "aws-project-1234"
-#     key    = "remote.tfstate"
-#     region = "us-east-1"
-# #     access_key = "AKIAW4TRIAUHCJ2BFTXB"
-# #     secret_key = "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"
-#     encrypt    = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "aws-project-1234"
+    key    = "remote.tfstate"
+    region = "us-east-1"
+    access_key = "AKIAW4TRIAUHCJ2BFTXB"
+    secret_key = "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"
+    encrypt    = true
+  }
+}
